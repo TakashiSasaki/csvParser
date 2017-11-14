fd9a4b33ca07f719a1484b81142daf15e4fd239c
Takashi SASAKI <takashi316@gmail.com>
2017-11-14 15:04:40 +0900

    add  detectEncoding to js.js

--- js.js
+++ js.js
@@ -1,2 +1,7 @@
-var hogehogehoge = require("./encoding.js/encoding.js");
+var encoding = require("./encoding.js/encoding.js");
+
+function detectEncoding(x){
+  var detectedEncoding = encoding.detect(x);
+  return detectedEncoding;
+}
 
