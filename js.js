404031c48efa92753b42b97633b588f1fc494d17
Takashi SASAKI <takashi316@gmail.com>
2017-11-14 15:08:47 +0900

    add module.exports to js.js

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 var encoding = require("./encoding.js/encoding.js");
-
-function detectEncoding(x){
-  var detectedEncoding = encoding.detect(x);
-  return detectedEncoding;
+module.exports = {
+  encoding: encoding
 }
+
 
