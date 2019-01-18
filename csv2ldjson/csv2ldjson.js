343eeae5a18966f4670210ebc39e1e6c8a542dac
Takashi SASAKI <takashi316@gmail.com>
2019-01-18 16:54:32 +0900

    cd csv2ldjson; make -B twolines.ldjson

--- csv2ldjson/csv2ldjson.js
+++ csv2ldjson/csv2ldjson.js
@@ -10,6 +10,9 @@ process.stdin.on('data', function(chunk) {
 process.stdin.on('end', function() {
   var parsed_array = csvParser.csvTextFile(input);
   var json_string = JSON.stringify(parsed_array);
-  console.log(json_string);
+  for(var i=0; i<parsed_array.length; ++i){
+    var json_string = JSON.stringify(parsed_array[i]);
+    console.log(json_string);
+  }//for
 });
 
