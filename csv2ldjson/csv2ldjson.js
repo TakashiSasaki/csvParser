26b7c0ca898c0b7578123b1a9afe6476b212fbfc
Takashi SASAKI <takashi316@gmail.com>
2019-01-18 17:03:26 +0900

    remove redundant json stringifying

--- csv2ldjson/csv2ldjson.js
+++ csv2ldjson/csv2ldjson.js
@@ -9,7 +9,6 @@ process.stdin.on('data', function(chunk) {
 });
 process.stdin.on('end', function() {
   var parsed_array = csvParser.csvTextFile(input);
-  var json_string = JSON.stringify(parsed_array);
   for(var i=0; i<parsed_array.length; ++i){
     var json_string = JSON.stringify(parsed_array[i]);
     console.log(json_string);
