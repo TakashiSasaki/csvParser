9beaa19e7cafbf579f5d07dc37a044903990dfe6
Takashi SASAKI <takashi316@gmail.com>
2019-01-18 17:21:37 +0900

    test for empty.tsv and oeline.tsv in test2ldjson cd tsv2ldjson; make -B

--- tsv2ldjson/tsv2ldjson.js
+++ tsv2ldjson/tsv2ldjson.js
@@ -9,9 +9,8 @@ process.stdin.on('data', function(chunk) {
 });
 process.stdin.on('end', function() {
   var parsed_array = csvParser.tsvTextFile(input);
-  var json_string = JSON.stringify(parsed_array);
   for(var i=0; i<parsed_array.length; ++i){
-    var ldjson = JSON.stringify(parsed_array[i]);
+    var json_string = JSON.stringify(parsed_array[i]);
     console.log(json_string);
   }//for
 });
