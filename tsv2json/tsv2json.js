e10cf4cf3577daeeff7b2b3cbc35eeff2bf7601e
Takashi SASAKI <takashi316@gmail.com>
2019-01-18 17:54:29 +0900

    cd tsv2json; make -B

---
+++ tsv2json/tsv2json.js
@@ -0,0 +1,15 @@
+process = require("process");
+csvParser = require("../CsvParser.js");
+
+var input = '';
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(chunk) {
+  input += chunk;
+});
+process.stdin.on('end', function() {
+  var parsed_array = csvParser.tsvTextFile(input);
+  var json_string = JSON.stringify(parsed_array);
+  console.log(json_string);
+});
+
